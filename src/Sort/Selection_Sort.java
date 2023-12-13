dfb17c2c1dfd61e76d518c8b21ff4992f5d4ff7a
SonyGod123 <jan.tschabrun@hotmail.com>
2023-12-13 13:27:23 +0100

    Selection_Sort

--- src/Sort/Selection_Sort.java
+++ src/Sort/Selection_Sort.java
@@ -6,17 +6,19 @@ public class Selection_Sort {
         public static void selectionSort(int[] arr) {
             int n = arr.length;
 
+
+
             for (int i = 0; i < n - 1; i++) {
                 int minIndex = i;
 
-                // Find the index of the minimum element in the unsorted part of the array
+                // Finde den Index des kleinsten Elements im unsortierten Teil des Arrays
                 for (int j = i + 1; j < n; j++) {
                     if (arr[j] < arr[minIndex]) {
                         minIndex = j;
                     }
                 }
 
-                // Swap the found minimum element with the first element of the unsorted part
+                // Das Element mit dem nächsten Größe wechseln
                 int temp = arr[minIndex];
                 arr[minIndex] = arr[i];
                 arr[i] = temp;
